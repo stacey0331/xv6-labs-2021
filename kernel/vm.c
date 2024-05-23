feef42dc939786e7de5112f1025587cc66f4fe7d
staceyl2 <staceyl2@illinois.edu>
2024-05-22 22:24:32 -0500

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,30 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_rec(pagetable_t pagetable, int level) {
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      // this PTE points to a lower-level page table.
+      for(int j = 0; j < level; j++) {
+        printf(".. ");
+      }
+      printf("..");
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+    
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        vmprint_rec((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
+
+void 
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_rec(pagetable, 0);
+}
