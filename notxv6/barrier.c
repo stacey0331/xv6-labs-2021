07eb010414312bdc064a700ac36cb06d8f89bdae
staceyl2 <staceyleetsaihsun@gmail.com>
2024-06-07 16:36:44 -0500

    finish thread

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,19 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread += 1;
+  if (bstate.nthread == nthread) {
+      bstate.round += 1;
+      bstate.nthread = 0;
+      pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    int prevRound = bstate.round;
+    while(prevRound == bstate.round) {
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
