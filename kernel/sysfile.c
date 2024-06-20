d9734a4580787975cc936e3b565d42caf6eb7060
staceyl2 <staceyleetsaihsun@gmail.com>
2024-06-19 20:27:37 -0500

    finish lab fs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,6 +309,36 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    if ((ip->type == T_SYMLINK) && ((omode & O_NOFOLLOW) == 0)) {
+      uint cnt = 0;
+      char parent[MAXPATH];
+      if (readi(ip, 0, (uint64)parent, 0, MAXPATH) != MAXPATH) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      iunlockput(ip);
+      if ((ip = namei(parent)) == 0) {
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      while (cnt < 10 && ip->type == T_SYMLINK) {
+        readi(ip, 0, (uint64)parent, 0, MAXPATH);
+        iunlockput(ip);
+        if ((ip = namei(parent)) == 0) {
+          end_op();
+          return -1;
+        }
+        cnt += 1;
+        ilock(ip);
+      }
+
+      if (cnt >= 10)  { // cycle approx.
+        end_op();
+        return -1; 
+      }
+    }
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -347,7 +377,7 @@ sys_open(void)
 
   iunlock(ip);
   end_op();
-
+  
   return fd;
 }
 
@@ -482,5 +512,25 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64 
+sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+  begin_op();
+  struct inode * ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+  if (writei(ip, 0, (uint64)target, 0, MAXPATH) != MAXPATH) {
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
